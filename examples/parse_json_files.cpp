c5a527760907f167c8f5edc9c852987ea26f77c3
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2022-05-04 15:42:32 +0100

    Rename and refactor example program.

--- examples/parse_json_files.cpp
+++ examples/parse_json_files.cpp
@@ -1,5 +1,5 @@
 //
-// Program: parse_json_file
+// Program: parse_json_files
 //
 // Description:
 //
@@ -27,11 +27,12 @@ using namespace JSONLib;
 // ========================
 // LOCAL TYPES/DEFINITIONS
 // ========================
+constexpr size_t kMaxFileLengthToDisplay = 16 * 1024;
 // ===============
 // LOCAL FUNCTIONS
 // ===============
 /// <summary>
-/// Prefix path to test data file name.
+/// Prefix current path to test data file name.
 /// </summary>
 /// <param name="name">Test data file name</param>
 /// <returns>Full path to test data file</returns>
@@ -40,32 +41,46 @@ std::string prefixTestDataPath(const std::string &file)
     std::filesystem::path currentPath = std::filesystem::current_path() / "testData" / file;
     return (currentPath.string());
 }
+/// <summary>
+/// Parse JSON file and display it reconstituted via stringify.
+/// </summary>
+/// <param name="fileName">JSON file name</param>
+void processJSONFile(const std::string &fileName)
+{
+    JSON json;
+    BufferDestination jsonDestination;
+    std::cout << fileName << "\n";
+    json.parse(FileSource{fileName});
+    json.stringify(jsonDestination);
+    if (jsonDestination.getBuffer().size() < kMaxFileLengthToDisplay)
+    {
+        std::cout << "[" << jsonDestination.getBuffer() << "]\n";
+    }
+    std::cout << "----------------------------OK\n";
+}
 // ============================
 // ===== MAIN ENTRY POINT =====
 // ============================
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
 {
-
     std::vector<std::string> fileList{"testfile001.json",
                                       "testfile002.json",
                                       "testfile003.json",
                                       "testfile004.json",
                                       "testfile005.json",
-                                      "large-file.json"};
-    JSON json;
+                                      "large-file.json" // Not kept in GitHub as 24Meg in size.
+                                      };
     //
-    // For each torrent file extract its information and display
+    // For each json parse it, stringify it and display unless its to large.
     //
     for (auto &fileName : fileList)
     {
         try
         {
-            BufferDestination jsonDestination;
-            std::cout << prefixTestDataPath(fileName) << "\n";
-            json.parse(FileSource{prefixTestDataPath(fileName)});
-            json.stringify(jsonDestination);
-            // std::cout << "[" << jsonDestination.getBuffer() << "]\n";
-            std::cout << "----------------------------OK\n";
+            if (std::string fullFileName = {prefixTestDataPath(fileName)}; std::filesystem::exists(fullFileName))
+            {
+                processJSONFile(fullFileName);
+            }
         }
         catch (std::exception &ex)
         {
