ac46944ea7ab4b4c3e2171cd61e1b79d43c9c1e3
clockworkengineer <robert_tizzard@hotmail.com>
2022-08-10 17:04:40 +0100

    Add comments to example.

--- examples/JSON_Display_Settings.cpp
+++ examples/JSON_Display_Settings.cpp
@@ -1,7 +1,10 @@
 //
 // Program: JSON_Display_Settings
 //
-// Description:
+// Description: Parse JSON example settings file and read its Bencode tree data
+// (BNode) and write out a textual representation of it; this is almost the same
+// as stringification but it is used to provide example code that traverses and
+// interperets the BNode tree data.
 //
 // Dependencies: C20++, PLOG, JSONLib.
 //
@@ -46,10 +49,15 @@ namespace fs = std::filesystem;
 std::string jsonFileDirectory() {
   return ((fs::current_path() / "files").string());
 }
+/// <summary>
+/// Process settings file top level object entry. This involves jusr reading the
+/// entries BNode data and logging it to a file.
+/// </summary>
 void processEntry(const json::Object::Entry &entry) {
-  std::string entryJSON;
-  PLOG_INFO << "[" << entry.getKey() << "]";
+  // Log main entry key
+  std::string entryJSON{"\n[" + entry.getKey() + "] = "};
   if (entry.getKey() == "files.exclude") {
+    // Read object data (key/boolean pair) and add to log
     entryJSON += "\n{\n";
     for (const auto &file : json::JRef<json::Object>(entry).objectEntries()) {
       entryJSON += "\"" + file.getKey() +
@@ -59,8 +67,10 @@ void processEntry(const json::Object::Entry &entry) {
     entryJSON.pop_back();
     entryJSON += "\n}";
   } else if (entry.getKey() == "explorerExclude.backup") {
+    // Read null data and add to log
     entryJSON += json::JRef<json::Null>(entry).toString();
   } else if (entry.getKey() == "cSpell.words") {
+    // Read array of string data and add to log
     entryJSON += "[";
     for (const auto &word : json::JRef<json::Array>(entry).array()) {
       entryJSON += "\"" + json::JRef<json::String>(word).toString() + "\",";
@@ -68,6 +78,7 @@ void processEntry(const json::Object::Entry &entry) {
     entryJSON.pop_back();
     entryJSON += "]";
   } else if (entry.getKey() == "files.associations") {
+    // Read object data (key/string pair) and add to log
     entryJSON += "\n{\n";
     for (const auto &file : json::JRef<json::Object>(entry).objectEntries()) {
       entryJSON += "\"" + file.getKey() + "\" : " + "\"" +
@@ -77,6 +88,7 @@ void processEntry(const json::Object::Entry &entry) {
     entryJSON.pop_back();
     entryJSON += "\n}";
   } else if (entry.getKey() == "C_Cpp.codeAnalysis.clangTidy.enabled") {
+    // Read boolean data and add to log
     entryJSON += json::JRef<json::Boolean>(entry).toString();
   } else {
     throw std::runtime_error("Invalid JSON settings file.");
@@ -91,14 +103,18 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
     // Initialise logging.
     plog::init(plog::debug, "JSON_Display_Settings.log");
     PLOG_INFO << "JSON_Display_Settings started ...";
+    // Log version
     PLOG_INFO << json::JSON().version();
+    // Parse in settings file
     const json::JSON json;
     json.parse(json::FileSource{
         (fs::path(jsonFileDirectory()) / "settings.json").string()});
     auto &settingsRoot = json.root();
+    // BNode root has to be an object
     if (settingsRoot.getType() != json::JNodeType::object) {
       throw std::runtime_error("Invalid JSON settings file.");
     }
+    // Loop and process each top level entry
     PLOG_INFO << "Displaying settings ...";
     for (const auto &entry :
          json::JRef<json::Object>(settingsRoot).objectEntries()) {
