7f69590b1290829132ea2fd96476139f93857b20
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2022-09-02 18:27:51 +0100

    Add more test files.

--- tests/JSONLib_Tests.hpp
+++ tests/JSONLib_Tests.hpp
@@ -54,6 +54,9 @@ std::string stripWhiteSpace(const std::string &jsonBuffer);
     "testfile009.json",                                          \
     "testfile010.json",                                          \
     "testfile011.json",                                          \
-    "testfile012.json" }))
+    "testfile012.json",                                          \
+    "testfile013.json",                                          \
+    "testfile014.json",                                          \
+    "testfile015.json" }))
 // Floating point comparison (accurate to within an epsilon)
 template<typename T> bool equalFloatingPoint(T a, T b, double epsilon) { return (std::fabs(a - b) <= epsilon); };
