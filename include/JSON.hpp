0a4f41bee8b8eae0d66014fb24b23b7bc4e1830c
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-11-13 16:28:53 +0000

    Snapshot.

--- include/JSON.hpp
+++ include/JSON.hpp
@@ -12,18 +12,10 @@ namespace JSON_Lib {
 
 #include "JSON_Forward.hpp"
 
-// struct Error : public std::runtime_error
-// {
-//   explicit Error(const std::string &message) : std::runtime_error("JSON Error: " + message) {}
-//   explicit Error(const std::pair<long, long> &position, const std::string &message = "")
-//     : std::runtime_error("JSON Error [Line: " + std::to_string(position.first)
-//                          + " Column: " + std::to_string(position.second) + "]: " + message)
-//   {}
-// };
-
 class JSON
 {
 public:
+  // JSON error
   struct Error : public std::runtime_error
   {
     explicit Error(const std::string &message) : std::runtime_error("JSON Error: " + message) {}
