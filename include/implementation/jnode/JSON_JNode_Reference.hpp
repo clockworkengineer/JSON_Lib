d0857ec57778c9971617842663839c183ec7272f
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2024-01-04 13:40:50 +0000

    Refactor.

--- include/implementation/jnode/JSON_JNode_Reference.hpp
+++ include/implementation/jnode/JSON_JNode_Reference.hpp
@@ -4,40 +4,40 @@ namespace JSON_Lib {
 // =========================
 // JNode reference converter
 // =========================
-template<typename T> void checkJNode(const Variant &jNodeVariant)
+template<typename T> void checkJNode(const JNode &jNode)
 {
   if constexpr (std::is_same_v<T, String>) {
-    if (!jNodeVariant.isString()) { throw JNode::Error("JNode not a string."); }
+    if (!jNode.isString()) { throw JNode::Error("JNode not a string."); }
   } else if constexpr (std::is_same_v<T, Number>) {
-    if (!jNodeVariant.isNumber()) { throw JNode::Error("JNode not a number."); }
+    if (!jNode.isNumber()) { throw JNode::Error("JNode not a number."); }
   } else if constexpr (std::is_same_v<T, Array>) {
-    if (!jNodeVariant.isArray()) { throw JNode::Error("JNode not an array."); }
+    if (!jNode.isArray()) { throw JNode::Error("JNode not an array."); }
   } else if constexpr (std::is_same_v<T, Object>) {
-    if (!jNodeVariant.isObject()) { throw JNode::Error("JNode not an object."); }
+    if (!jNode.isObject()) { throw JNode::Error("JNode not an object."); }
   } else if constexpr (std::is_same_v<T, Boolean>) {
-    if (!jNodeVariant.isBoolean()) { throw JNode::Error("JNode not a boolean."); }
+    if (!jNode.isBoolean()) { throw JNode::Error("JNode not a boolean."); }
   } else if constexpr (std::is_same_v<T, Null>) {
-    if (!jNodeVariant.isNull()) { throw JNode::Error("JNode not a null."); }
+    if (!jNode.isNull()) { throw JNode::Error("JNode not a null."); }
   }
 }
 template<typename T> T &JRef(JNode &jNode)
 {
-  checkJNode<T>(jNode.getVariant());
+  checkJNode<T>(jNode);
   return (static_cast<T &>(jNode.getVariant()));
 }
 template<typename T> const T &JRef(const JNode &jNode)
 {
-  checkJNode<T>(jNode.getVariant());
+  checkJNode<T>(jNode);
   return (static_cast<const T &>(jNode.getVariant()));
 }
 template<typename T> T &JRef(Object::Entry &jNodeEntry)
 {
-  checkJNode<T>(jNodeEntry.getJNode().getVariant());
+  checkJNode<T>(jNodeEntry.getJNode());
   return (static_cast<T &>(jNodeEntry.getJNode().getVariant()));
 }
 template<typename T> const T &JRef(const Object::Entry &jNodeEntry)
 {
-  checkJNode<T>(jNodeEntry.getJNode().getVariant());
+  checkJNode<T>(jNodeEntry.getJNode());
   return (static_cast<const T &>(jNodeEntry.getJNode().getVariant()));
 }
 }// namespace JSON_Lib
