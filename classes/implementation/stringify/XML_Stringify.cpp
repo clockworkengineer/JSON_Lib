6f53e5cd791ef53564544ce1b9888f39bab86b85
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2024-01-17 14:46:56 +0000

    Add more tests.

---
+++ classes/implementation/stringify/XML_Stringify.cpp
@@ -0,0 +1,132 @@
+//
+// Class: XML_Stringify
+//
+// Description: Stringify JNode tree to Bencode on destination stream.
+//
+// Dependencies: C++20 - Language standard features used.
+//
+
+#include "JSON.hpp"
+#include "JSON_Core.hpp"
+
+#include "XML_Stringify.hpp"
+
+namespace JSON_Lib {
+
+/// <summary>
+/// Convert Number JNode to Bencode on destination stream.
+/// </summary>
+/// <param name="jNode">Number JNode.</param>
+/// <param name="destination">Destination stream for Bencode.</param>
+void XML_Stringify::stringifyNumber(const JNode &jNode, IDestination &destination) const
+{
+  // All numbers to rounded integers
+  destination.add("i" + std::to_string(JRef<Number>(jNode).value<int>()) + "e");
+}
+
+/// <summary>
+/// Convert String JNode to Bencode on destination stream.
+/// </summary>
+/// <param name="jNode">String JNode.</param>
+/// <param name="destination">Destination stream for Bencode.</param>
+void XML_Stringify::stringifyString(const JNode &jNode, IDestination &destination) const
+{
+  destination.add(
+    std::to_string(static_cast<int>(JRef<String>(jNode).value().length())) + ":" + JRef<String>(jNode).value());
+}
+
+/// <summary>
+/// Convert Boolean JNode to Bencode on destination stream.
+/// </summary>
+/// <param name="jNode">Boolean JNode.</param>
+/// <param name="destination">Destination stream for Bencode.</param>
+void XML_Stringify::stringifyBoolean(const JNode &jNode, IDestination &destination) const
+{
+  if (JRef<Boolean>(jNode).value()) {
+    destination.add("4:True");
+  } else {
+    destination.add("5:False");
+  }
+}
+
+/// <summary>
+/// Convert Null JNode to Bencode on destination stream.
+/// </summary>
+/// <param name="jNode">Null JNode.</param>
+/// <param name="destination">Destination stream for Bencode.</param>
+void XML_Stringify::stringifyNull(const JNode &jNode, IDestination &destination) const
+{
+  destination.add("4:null");
+}
+
+/// <summary>
+/// Convert Hole JNode to Bencode on destination stream.
+/// </summary>
+/// <param name="jNode">Hole JNode.</param>
+/// <param name="destination">Destination stream for Bencode.</param>
+void XML_Stringify::stringifyHole(const JNode &jNode, IDestination &destination) const {}
+
+/// <summary>
+/// Convert Object JNode to Bencode on destination stream.
+/// </summary>
+/// <param name="jNode">Object JNode.</param>
+/// <param name="destination">Destination stream for Bencode.</param>
+/// <param name="indent">Current print indentation.</param>
+void XML_Stringify::stringifyObject(const JNode &jNode, IDestination &destination, long indent) const
+{
+  destination.add('d');
+  for (auto &entry : JRef<Object>(jNode).value()) {
+    stringifyString(entry.getKey(), destination);
+    stringify(entry.getJNode(), destination, 0);
+  }
+  destination.add("e");
+}
+
+/// <summary>
+/// Convert Array JNode to Bencode on destination stream.
+/// </summary>
+/// <param name="jNode">Array JNode.</param>
+/// <param name="destination">Destination stream for Bencode.</param>
+/// <param name="indent">Current print indentation.</param>
+void XML_Stringify::stringifyArray(const JNode &jNode, IDestination &destination, long indent) const
+{
+  destination.add('l');
+  if (!JRef<Array>(jNode).value().empty()) {
+    for (auto &entry : JRef<Array>(jNode).value()) { stringify(entry, destination, 0); }
+  }
+  destination.add("e");
+}
+
+/// <summary>
+/// Recursively traverse JNode structure encoding it into JSON string on
+/// the destination stream passed in.
+/// </summary>
+/// <param name=jNode>JNode structure to be traversed.</param>
+/// <param name=destination>Destination stream for stringified JSON.</param>
+/// <param name="indent">Current print indentation.</param>
+void XML_Stringify::stringify(const JNode &jNode, IDestination &destination, long indent) const
+{
+  if (jNode.isNumber()) {
+    stringifyNumber(jNode, destination);
+  } else if (jNode.isString()) {
+    stringifyString(jNode, destination);
+  } else if (jNode.isBoolean()) {
+    stringifyBoolean(jNode, destination);
+  } else if (jNode.isNull()) {
+    stringifyNull(jNode, destination);
+  } else if (jNode.isHole()) {
+    stringifyHole(jNode, destination);
+  } else if (jNode.isObject()) {
+    stringifyObject(jNode, destination, indent);
+  } else if (jNode.isArray()) {
+    stringifyArray(jNode, destination, indent);
+  } else {
+    throw JSON::Error("Unknown JNode type encountered during stringification.");
+  }
+}
+
+long XML_Stringify::getIndent() const { return (0); }
+
+void XML_Stringify::setIndent(long indent) {}
+
+}// namespace JSON_Lib
---
+++ include/implementation/stringify/XML_Stringify.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "JSON.hpp"
+#include "JSON_Core.hpp"
+#include "JSON_Sources.hpp"
+#include "JSON_Destinations.hpp"
+
+namespace JSON_Lib {
+
+class XML_Stringify : public IStringify
+{
+public:
+  XML_Stringify() = default;
+  XML_Stringify(const XML_Stringify &other) = delete;
+  XML_Stringify &operator=(const XML_Stringify &other) = delete;
+  XML_Stringify(XML_Stringify &&other) = delete;
+  XML_Stringify &operator=(XML_Stringify &&other) = delete;
+  ~XML_Stringify() = default;
+
+  virtual void stringify(const JNode &jNode, IDestination &destination, long indent) const override;
+  virtual long getIndent() const override;
+  virtual void setIndent(long indent) override;
+
+private:
+  // Stringify Bencode
+  void stringifyNumber(const JNode &jNode, IDestination &destination) const;
+  void stringifyString(const JNode &jNode, IDestination &destination) const;
+  void stringifyBoolean(const JNode &jNode, IDestination &destination) const;
+  void stringifyNull(const JNode &jNode, IDestination &destination) const;
+  void stringifyHole(const JNode &jNode, IDestination &destination) const;
+  void stringifyObject(const JNode &jNode, IDestination &destination, long indent) const;
+  void stringifyArray(const JNode &jNode, IDestination &destination, long indent) const;
+};
+
+}// namespace JSON_Lib
