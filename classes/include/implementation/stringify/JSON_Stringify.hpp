92e57c38d33880a02e63913f2a49b6d73ae666df
clockworkengineer <robert_tizzard@hotmail.com>
2024-05-23 21:12:06 +0100

    refactor stringifiers.

--- classes/include/implementation/stringify/JSON_Stringify.hpp
+++ classes/include/implementation/stringify/JSON_Stringify.hpp
@@ -17,7 +17,72 @@ public:
   JSON_Stringify &operator=(JSON_Stringify &&other) = delete;
   ~JSON_Stringify() override = default;
 
-  void stringify(const JNode &jNode, IDestination &destination, long indent) const override;
+/// <summary>
+/// Recursively traverse JNode structure encoding it into JSON string on
+/// the destination stream passed in.
+/// </summary>
+/// <param name="jNode">JNode structure to be traversed.</param>
+/// <param name="destination">Destination stream for stringified JSON.</param>
+/// <param name="indent">Current print indentation.</param>
+void stringify(const JNode &jNode, IDestination &destination, const long indent) const override
+  {
+  if (jNode.isNumber()) {
+    destination.add(JRef<Number>(jNode).toString());
+  } else if (jNode.isString()) {
+    destination.add('"');
+    destination.add(jsonTranslator.to(JRef<String>(jNode).toString()));
+    destination.add('"');
+  } else if (jNode.isBoolean()) {
+    destination.add(JRef<Boolean>(jNode).toString());
+  } else if (jNode.isNull()) {
+    destination.add(JRef<Null>(jNode).toString());
+  } else if (jNode.isHole()) {
+    destination.add(JRef<Hole>(jNode).toString());
+  } else if (jNode.isObject()) {
+    size_t commaCount = JRef<Object>(jNode).value().size() - 1;
+    destination.add('{');
+    if (indent != 0) { destination.add('\n'); }
+    for (auto &entry : JRef<Object>(jNode).value()) {
+      if (indent != 0) { destination.add(std::string(indent, ' ')); }
+      destination.add('"');
+      destination.add(jsonTranslator.to(JRef<String>(entry.getKey()).toString()));
+      destination.add('"');
+      destination.add(":");
+      if (indent != 0) { destination.add(" "); }
+      stringify(entry.getJNode(), destination, indent != 0 ? indent + printIndent : 0);
+      if (commaCount-- > 0) {
+        destination.add(",");
+        if (indent != 0) { destination.add('\n'); }
+      }
+    }
+    if (indent != 0) {
+      destination.add('\n');
+      destination.add(std::string(indent - printIndent, ' '));
+    }
+    destination.add("}");
+  } else if (jNode.isArray()) {
+    destination.add('[');
+    if (!JRef<Array>(jNode).value().empty()) {
+      size_t commaCount = JRef<Array>(jNode).value().size() - 1;
+      if (indent != 0) { destination.add('\n'); }
+      for (auto &entry : JRef<Array>(jNode).value()) {
+        if (indent != 0) { destination.add(std::string(indent, ' ')); }
+        stringify(entry, destination, indent != 0 ? indent + printIndent : 0);
+        if (commaCount-- > 0) {
+          destination.add(",");
+          if (indent != 0) { destination.add('\n'); }
+        }
+      }
+      if (indent != 0) {
+        destination.add('\n');
+        destination.add(std::string(indent - printIndent, ' '));
+      }
+    }
+    destination.add("]");
+  } else {
+    throw Error("Unknown JNode type encountered during stringification.");
+  }
+}
 
   // Set print ident value
   void setIndent(const long indent) override
@@ -26,6 +91,7 @@ public:
     printIndent = indent;
   }
 
+  //  Set indent value
   long getIndent() const override { return printIndent; }
 
 private:
--- classes/include/implementation/stringify/XML_Stringify.hpp
+++ classes/include/implementation/stringify/XML_Stringify.hpp
@@ -18,16 +18,68 @@ public:
   XML_Stringify &operator=(XML_Stringify &&other) = delete;
   ~XML_Stringify() override = default;
 
-  void stringify(const JNode &jNode, IDestination &destination, long indent) const override;
-  [[nodiscard]] long getIndent() const override;
-  void setIndent(long indent) override;
+  /// <summary>
+  /// Recursively traverse JNode structure encoding it into JSON string on
+  /// the destination stream passed in.
+  /// </summary>
+  /// <param name="jNode">JNode structure to be traversed.</param>
+  /// <param name="destination">Destination stream for stringified JSON.</param>
+  /// <param name="indent">Current print indentation.</param>
+  void stringify(const JNode &jNode, IDestination &destination, [[maybe_unused]] long indent) const override
+  {
+    destination.add(R"(<?xml version="1.0" encoding="UTF-8"?>)");
+    destination.add("<root>");
+    stringifyXML(jNode, destination, 0);
+    destination.add("</root>");
+  }
+
+  [[nodiscard]] long getIndent() const override { return 0; }
+  void setIndent([[maybe_unused]] long indent) override {}
 
 private:
-  // Stringify Bencode to XML
-  void stringifyXML(const JNode &jNode, IDestination &destination, long indent) const;
+  /// <summary>
+  /// Recursively traverse JNode structure encoding it into XML string on
+  /// the destination stream passed in.
+  /// </summary>
+  /// <param name="jNode">JNode structure to be traversed.</param>
+  /// <param name="destination">Destination stream for stringified XML.</param>
+  /// <param name="indent">Current print indentation.</param>
+  void stringifyXML(const JNode &jNode, IDestination &destination, const long indent) const
+  {
+    if (jNode.isNumber()) {
+      destination.add(std::to_string(JRef<Number>(jNode).value<int>()));
+    } else if (jNode.isString()) {
+      destination.add(xmlTranslator.to(JRef<String>(jNode).value()));
+    } else if (jNode.isBoolean()) {
+      if (JRef<Boolean>(jNode).value()) {
+        destination.add("True");
+      } else {
+        destination.add("False");
+      }
+    } else if (jNode.isNull()||jNode.isHole()) {
+      ;
+    } else if (jNode.isObject()) {
+      for (const auto &jNodeNext : JRef<Object>(jNode).value()) {
+        auto elementName = JRef<String>(jNodeNext.getKey()).value();
+        std::ranges::replace(elementName, ' ', '-');
+        destination.add("<" + elementName + ">");
+        stringifyXML(jNodeNext.getJNode(), destination, 0);
+        destination.add("</" + elementName + ">");
+      }
+    } else if (jNode.isArray()) {
+      if (JRef<Array>(jNode).value().size() > 1) {
+        for (const auto &bNodeNext : JRef<Array>(jNode).value()) {
+          destination.add("<Row>");
+          stringifyXML(bNodeNext, destination, 0);
+          destination.add("</Row>");
+        }
+      }
+    } else {
+      throw Error("Unknown JNode type encountered during stringification.");
+    }
+  }
 
   XML_Translator xmlTranslator;
-  
 };
 
 }// namespace JSON_Lib
