e700c1436e7334ff1372e4c661a933acf8b95e1f
clockworkengineer <robert_tizzard@hotmail.com>
2024-06-26 18:56:24 +0100

    Refactor

--- classes/include/implementation/variants/JSON_Object.hpp
+++ classes/include/implementation/variants/JSON_Object.hpp
@@ -8,9 +8,9 @@
 namespace JSON_Lib {
 
 // Object error
-struct ObjectError final : std::runtime_error {
-  explicit ObjectError(const std::string &message)
-    : std::runtime_error("Object Error: " + message) {}
+struct ObjectError final : std::runtime_error
+{
+  explicit ObjectError(const std::string &message) : std::runtime_error("Object Error: " + message) {}
 };
 
 // Object entry
@@ -41,9 +41,7 @@ struct Object : Variant
   Object &operator=(Object &&other) = default;
   ~Object() = default;
   // Add Entry to Object
-  template <typename T> void add(T &&entry) {
-    jNodeObject.emplace_back(std::forward<T>(entry));
-  }
+  template<typename T> void add(T &&entry) { jNodeObject.emplace_back(std::forward<T>(entry)); }
   // Return true if an object contains a given key
   [[nodiscard]] bool contains(const std::string &key) const
   {
@@ -57,32 +55,31 @@ struct Object : Variant
   // Return number of entries in an object
   [[nodiscard]] int size() const { return static_cast<int>(jNodeObject.size()); }
   // Return object entry for a given key
-  JNode &operator[](const std::string &key) { return findKey(jNodeObject,key)->getJNode(); }
-  const JNode &operator[](const std::string &key) const { return findKey(jNodeObject,key)->getJNode(); }
+  JNode &operator[](const std::string &key) { return findKey(jNodeObject, key)->getJNode(); }
+  const JNode &operator[](const std::string &key) const { return findKey(jNodeObject, key)->getJNode(); }
   // Return reference to base of object entries
   Entries &value() { return jNodeObject; }
   [[nodiscard]] const Entries &value() const { return jNodeObject; }
 
 private:
   // Search for a given entry given a key and object list
-  [[nodiscard]] static Entries::iterator findKey(Entries &object,const std::string &key);
-  [[nodiscard]] static Entries::const_iterator findKey(const Entries &object, const std::string &key) ;
+  [[nodiscard]] static Entries::iterator findKey(Entries &object, const std::string &key);
+  [[nodiscard]] static Entries::const_iterator findKey(const Entries &object, const std::string &key);
 
   // Object entries list
   Entries jNodeObject;
 };
 
-inline Object::Entries::iterator Object::findKey(Entries &object,const std::string &key)
+inline Object::Entries::iterator Object::findKey(Entries &object, const std::string &key)
 {
-  const auto it = std::ranges::find_if(object, [&key](Entry &entry) -> bool {
-    return static_cast<String &>(entry.getKey().getVariant()).value() == key;
-  });
+  auto it = std::ranges::find_if(
+    object, [&key](Entry &entry) -> bool { return static_cast<String &>(entry.getKey().getVariant()).value() == key; });
   if (it == object.end()) { throw Object::Error("Invalid key used to access object."); }
   return it;
 }
 inline Object::Entries::const_iterator Object::findKey(const Entries &object, const std::string &key)
 {
-  const auto it = std::ranges::find_if(object, [&key](const Entry &entry) -> bool {
+  auto it = std::ranges::find_if(object, [&key](const Entry &entry) -> bool {
     return static_cast<const String &>(entry.getKey().getVariant()).value() == key;
   });
   if (it == object.end()) { throw Object::Error("Invalid key used to access object."); }
