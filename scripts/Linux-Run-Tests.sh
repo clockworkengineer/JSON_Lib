f49c480fdf1677ccfa8484c1bd71ad3a543315b7
clockworkengineer <robert_tizzard@hotmail.com>
2024-07-31 12:56:48 +0100

    Get test program name right.

--- scripts/Linux-Run-Tests.sh
+++ scripts/Linux-Run-Tests.sh
@@ -1,13 +1,13 @@
  #!/bin/bash
  cd ./Release/tests/
- ./Bencode_Lib_Unit_Tests
+ ./JSON_Lib_Unit_Tests
 if [ $? -eq 0 ]; then
   cd ../../
 else
   exit $?
 fi
  cd ./Debug/tests/
- ./Bencode_Lib_Unit_Tests
+ ./JSON_Lib_Unit_Tests
 if [ $? -eq 0 ]; then
   cd ../../
 else
