024ea4b587af5e8a44e240af08f61837ef656b2b
clockworkengineer <robert_tizzard@hotmail.com>
2025-05-30 17:47:22 +0100

    refactor converter.

--- classes/include/implementation/converter/JSON_Converter.hpp
+++ classes/include/implementation/converter/JSON_Converter.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 namespace JSON_Lib {
-
+[[nodiscard]] std::string toUtf8(char16_t utf16);
 [[nodiscard]] std::u16string toUtf16(const std::string_view &utf8);
 [[nodiscard]] std::string toUtf8(const std::u16string_view &utf16);
-
 }// namespace JSON_Lib
